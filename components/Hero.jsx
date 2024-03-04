1884afd2e8208280f0e1de6c5d32c49ee843de7f
KeithCheng22 <keithcheng.sgs@gmail.com>
2024-03-04 23:50:26 +0800

    change hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,18 +5,17 @@ import Netflix from '../public/netflix.png'
 const Hero = () => {
   return (
     <header id='header'>
-        <section className="relative max-h-[70vh] flex">
-            <video className="opacity-35 rounded-xl object-cover" muted autoPlay loop src={require('../video.mp4')} />;
+        <section className="relative bg-right bg-cover bg-[url('../public/hero.jpeg')] py-8 px-2">
 
-            <div className='absolute flex px-2 py-6 items-center'>
-              <Image src={Netflix} width={60}/>
+            <div className='relative flex items-center'>
+              <Image src={Netflix} width={60} alt='netflick logo'/>
               <h1 className='text-white text-4xl font-bold'>NetFlick</h1>
             </div>
 
-            <div className="absolute inset-0 h-fit my-auto text-white text-center">
+            <div className="mt-3 relative h-fit my-auto text-white text-center">
                 <h1 className='w-[80%] mx-auto  max-sm:text-lg sm:text-2xl md:text-4xl lg:text-6xl font-semibold'>All the movies you <span className='text-red-300'>love</span> in one place</h1>
                 <form>
-                  <input type='text' name='query' autoFocus className='w-[80%] min-h-10 rounded-xl bg-transparent mt-5 border-2 indent-4 outline-transparent' placeholder='Marvels: Infinity War'></input>
+                  <input type='text' name='query' autoFocus className='w-[80%] min-h-10 rounded-xl bg-transparent mt-5 border-2 indent-4 outline-transparent placeholder:text-white' placeholder='Marvels: Infinity War'></input>
                 </form>
             </div>
         </section>
--- components/MovieDetails.jsx
+++ components/MovieDetails.jsx
@@ -14,11 +14,11 @@ const MovieDetails = ({ movieDesc }) => {
 
   return (
     <div className='text-white p-4 max-w-[1200px] md:flex md:items-center md:h-[100vh] md:gap-6 bg-white bg-opacity-30 shadow-[0_8px_32px_0_rgba( 31, 38, 135, 0.37 )] backdrop-blur-[4px] border border-white/20 rounded-lg'>
-      <div className=''>
-        <Image src={imageUrl} width={300} height={50} className='rounded-lg' alt={`${title} poster`}/>
+      <div className='md:w-[50%]'>
+        <Image src={imageUrl} width={300} height={50} className='rounded-lg md:mx-auto' alt={`${title} poster`}/>
       </div>
 
-      <section>
+      <section className='md:w-[35%]'>
       <h1 className={`text-white text-xl tracking-[0.08em] font-semibold mt-3 ${bebas.className}`}>{title}</h1>
       <p className={`px-2 bg-white bg-opacity-50 rounded-md w-fit ${bebas.className}`}>{original_language}</p>
       <p className='mt-3'><span className='font-semibold'>Sypnosis: </span>{overview}</p>
