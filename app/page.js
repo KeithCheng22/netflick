b03b1f8f154fd15f934e694642cb20d57e7453d5
KeithCheng22 <keithcheng.sgs@gmail.com>
2024-03-04 22:17:02 +0800

    fix suspense issue

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import LoadMore from "@/components/LoadMore";
 import Button from "@/components/Button";
 import Hero from "@/components/Hero";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <>
       <Hero />
       <main className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2">
+        <Suspense>
         <LoadMore />
+        </Suspense>
       </main>
       <Button />
     </>
