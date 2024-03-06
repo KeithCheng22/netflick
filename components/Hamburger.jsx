32954814ecd1a4bcad1fd9140c93860896aec7d1
KeithCheng22 <keithcheng.sgs@gmail.com>
2024-03-06 19:25:05 +0800

    fix hamburger menu

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -2,11 +2,7 @@
 import { useState } from "react"
 import Link from "next/link"
 
-const Hamburger = () => {
-    const [expanded, setExpanded] = useState(false)
-    const toggle = () => {
-        setExpanded(prev => !prev)
-    }
+const Hamburger = ({expanded, toggle}) => {
   return (
     <>
     <div className="z-10">
@@ -14,11 +10,6 @@ const Hamburger = () => {
             <div className={`w-[30px] h-[4px] bg-white  transition-all duration-500 ${expanded ? '-rotate-45 origin-right -translate-y-[0.35rem]' : 'block'}`}></div>
             <div className={`w-[30px] h-[4px] bg-white mt-1 transition-all duration-500 ${expanded ? 'opacity-0' : 'opacity-1'}`}></div>
             <div className={`w-[30px] h-[4px] bg-white mt-1 transition-all duration-500 ${expanded ? 'rotate-45 origin-right ' : 'block'}`}></div>
-
-            <div className={`transition-all duration-[5000] flex gap-2 p-1 ${expanded ? 'absolute max-h-[300px] sm:hidden inset-0 z-1 bg-white/30' : 'hidden'}`}>
-              <Link className='text-white text-2xl' href='/'>Home</Link>
-              <Link className='text-white text-2xl' href='/feedback'>Feedback</Link>
-            </div>
       </div>
     </div>
 
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,18 +1,35 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import Netflix from '../public/netflix.png'
 import Hamburger from './Hamburger'
+import { useState } from "react"
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false)
+  const toggle = () => {
+      setExpanded(prev => !prev)
+  }
+
   return (
-    <nav className='flex items-center justify-between mb-5'>
+    <nav className={`flex flex-col mb-5 sm:flex-row sm:justify-between sm:items-center`}>
+
+      <div className='flex justify-between items-center'>
         <div className='flex items-center'>
             <Image src={Netflix} width={60} alt='netflick logo'/>
             <h1 className='text-white text-4xl font-bold'>NetFlick</h1>
         </div>
         
-        <Hamburger />
+        <Hamburger expanded={expanded} toggle={toggle}/>
+      </div>
+
+        <menu className={`sm:hidden overflow-hidden transition-all duration-500 ease-in-out ${expanded ? 'max-h-[200px]' : 'max-h-0'} mt-2 ml-2`}>
+          <Link onClick={() => setExpanded(false)} className='text-white block font-semibold text-xl hover:text-white/80' href='/'>Home</Link>
+          <Link onClick={() => setExpanded(false)} className='text-white block font-semibold text-xl hover:text-white/80' href='/feedback'>Feedback</Link>
+        </menu>
+
         <menu className='text-white flex gap-5 max-sm:hidden text-2xl'>
             <Link className='outline py-1 px-2 rounded-md hover:bg-transparent hover:text-white bg-white text-black/90 transition-all duration-300' href='/'>Home</Link>
             <Link className='outline py-1 px-2 rounded-md hover:bg-white hover:text-black transition-all duration-300' href='/feedback'>Feedback</Link>
